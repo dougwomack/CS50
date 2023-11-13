aa6feab792f53fa061002756efd3b92b6dd5fa5b
dougwomack <doug@shutterchimp.com>
2023-11-13 10:38:51 -0600

    continuing lessons

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print ("Hello World!")
+# Ask user for their name
+name = input("What's your name? ")
+
+# Strip whitespace and title case name
+name = name.strip().title()
+
+# Split first name and last name
+first, last = name.split(" ")
+
+# Say hello to user
+print ("Hello " + first)
+print ("Hello " + first, last, end=" :-)\n")
+print (f"Hello {first} {last}")
